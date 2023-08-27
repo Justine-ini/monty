c7cd57248008488c3c7d17b719539af2d04bff02
Justine-ini <inijustine4040@gmail.com>
2023-08-26 23:44:41 -0700

    monty

--- rotl.c
+++ rotl.c
@@ -1,26 +1,26 @@
 #include "monty.h"
 /**
-  *f_rotl- rotates the stack to the top
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
+ * rotl - rotates the stack to the top
+ * @head: double pointer to the begining of the list
+ * @counter: script line number
  */
-void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+void rotl(stack_t **head, unsigned int counter)
 {
-	stack_t *tmp = *head, *aux;
+        stack_t *temp, *top;
+        (void) counter;
 
-	if (*head == NULL || (*head)->next == NULL)
-	{
-		return;
-	}
-	aux = (*head)->next;
-	aux->prev = NULL;
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-	tmp->next = *head;
-	(*head)->next = NULL;
-	(*head)->prev = tmp;
-	(*head) = aux;
+        if (!head || !(*head) || !(*head)->next)
+                return;
+        temp = *head;
+        top = temp->next;
+        top->prev = NULL;
+
+        while (temp->next != NULL)
+                temp = temp->next;
+        temp->next = *head;
+
+        (*head)->next = NULL;
+        (*head)->prev = temp;
+
+        *head = top;
 }
